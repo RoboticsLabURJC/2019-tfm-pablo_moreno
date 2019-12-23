9723e452566c3cee6470970d9792107cf34d97a6
pmvera <pablomvera14@gmail.com>
2019-12-23 19:07:48 +0100

    Added first post and personal info

---
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "Ecuations",
+        "excerpt":"How do you use them?   The equations admitted are those usually used in Latex and Markdown. Thanks to the mathjax library they are rendered.     Thanks to this library and the integration with Minimal Mistakes, the result of your documentation will look spectacular.  ","categories": ["your category"],
+        "tags": ["tag 1","tag 2","tag 3","tag 4"],
+        "url": "http://localhost:4000/your%20category/ecuations/",
+        "teaser":null},{
+        "title": "My first post",
+        "excerpt":"Simple text to see the appearance and result of the documentation in a post. Title 1 Lorem ipsum dolor sit amet consectetur adipiscing elit odio, ullamcorper orci torquent tristique habitasse interdum integer ridiculus, lectus pellentesque mus donec morbi sapien molestie. Libero platea vivamus tortor fringilla vel penatibus ut egestas, etiam...","categories": ["your category"],
+        "tags": ["tag 1","tag 2","tag 3","tag 4"],
+        "url": "http://localhost:4000/your%20category/previous-work/",
+        "teaser":null},{
+        "title": "Week 0. Let's start",
+        "excerpt":"Hi everybody! I’m Pablo Moreno Vera, student of the master in Telecommunication Engineering at Rey Juan Carlos University. I’m starting at blogging with this first post. I didn’t try it before and I hope post every week. My Post-Degree Project is about Klibotics and Unibotics platforms. On my first week...","categories": ["Previous work"],
+        "tags": ["logbook","first steps","week 0","setting environment"],
+        "url": "http://localhost:4000/previous%20work/week-0/",
+        "teaser":null}]
