1a0aac02d0e691af861918c1c6eb4fcca0e54e7a
pmvera <pablomvera14@gmail.com>
2020-01-09 13:10:17 +0100

    Adding second post to wiki

--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -13,4 +13,9 @@ var store = [{
         "excerpt":"Hi everybody! I’m Pablo Moreno Vera, student of the master in Telecommunication Engineering at Rey Juan Carlos University. I’m starting at blogging with this first post. I didn’t try it before and I hope post every week. My Post-Degree Project is about Klibotics and Unibotics platforms. On my first week...","categories": ["Previous work"],
         "tags": ["logbook","first steps","week 0","setting environment"],
         "url": "http://localhost:4000/previous%20work/week-0/",
+        "teaser":null},{
+        "title": "Week 1. Screen-sharing & WebSockets",
+        "excerpt":"Hi again! On my first week doing different stuff I had to read a TFG of one of my partners named Rubén Álvarez. He made an amazing job and explained so much interesting things about kibotics organization, wich is where I belong. Also, I had to test some code about...","categories": ["Previous work"],
+        "tags": ["logbook","first steps","week 0","setting environment"],
+        "url": "http://localhost:4000/previous%20work/week-1/",
         "teaser":null}]
