f010bf0097ba5ed19d75d0831dc0a4f8f6345f16
pmvera <pablomvera14@gmail.com>
2020-01-09 13:46:16 +0100

    Updated posts code

--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -14,7 +14,7 @@ var store = [{
         "tags": ["logbook","first steps","week 0","setting environment"],
         "url": "http://localhost:4000/previous%20work/week-0/",
         "teaser":null},{
-        "title": "Week 1. Screen-sharing & WebSockets",
+        "title": "Week 1. Screen-sharing",
         "excerpt":"Hi again! On my first week doing different stuff I had to read a TFG of one of my partners named Rubén Álvarez. He made an amazing job and explained so much interesting things about kibotics organization, wich is where I belong. Also, I had to test some code about...","categories": ["Previous work"],
         "tags": ["logbook","first steps","week 0","setting environment"],
         "url": "http://localhost:4000/previous%20work/week-1/",
